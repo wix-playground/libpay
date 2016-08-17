c2e0c37186e6a8b75cc28a708cf6d1fac68155a7
Raz, Ohad <ohadr@wix.com>
2016-08-17 17:20:26 +0300

    exceptions that takes cause as the parameter, if given, should use the cause.toString as the message

--- libpay-api/src/main/scala/com/wix/pay/PaymentException.scala
+++ libpay-api/src/main/scala/com/wix/pay/PaymentException.scala
@@ -8,13 +8,50 @@ package com.wix.pay
 
 
 /** An exception raised upon failure processing a payment transaction. */
-class PaymentException(message: String = null, cause: Throwable = null) extends RuntimeException(message, cause)
+class PaymentException(message: String, cause: Throwable) extends RuntimeException(message, cause)
+
+/** The Companion Object of the [[PaymentException]] class, which introduces means for instantiating an exception
+  * object.
+  *
+  * @author <a href="mailto:ohadr@wix.com">Raz, Ohad</a>
+  */
+object PaymentException {
+  def apply(): PaymentException = new PaymentException(null, null)
+  def apply(message: String): PaymentException = new PaymentException(message, null)
+  def apply(cause: Throwable): PaymentException = new PaymentException(Option(cause).map(_.toString).orNull, cause)
+}
+
 
 
 /** An exception raised upon rejecting a payment transaction. */
-case class PaymentRejectedException(message: String = null,
-                                    cause: Throwable = null) extends PaymentException(message, cause)
+case class PaymentRejectedException(message: String, cause: Throwable) extends PaymentException(message, cause)
+
+/** The Companion Object of the [[PaymentRejectedException]] class, which introduces means for instantiating an
+  * exception object.
+  *
+  * @author <a href="mailto:ohadr@wix.com">Raz, Ohad</a>
+  */
+object PaymentRejectedException {
+  def apply(): PaymentRejectedException = this(null, null)
+  def apply(message: String): PaymentRejectedException = this(message, null)
+  def apply(cause: Throwable): PaymentRejectedException = this(Option(cause).map(_.toString).orNull, cause)
+}
 
 
-/** An exception raised upon encountering an error by the payment gateway (e.g., in the case of an invalid credentials). */
-case class PaymentErrorException(message: String = null, cause: Throwable = null) extends PaymentException(message, cause)
+
+/** An exception raised upon encountering an error by the payment gateway (e.g., in the case of an invalid
+  * credentials).
+  */
+case class PaymentErrorException(message: String, cause: Throwable) extends PaymentException(message, cause)
+
+
+/** The Companion Object of the [[PaymentErrorException]] class, which introduces means for instantiating an exception
+  * object.
+  *
+  * @author <a href="mailto:ohadr@wix.com">Raz, Ohad</a>
+  */
+object PaymentErrorException {
+  def apply(): PaymentErrorException = this(null, null)
+  def apply(message: String): PaymentErrorException = this(message, null)
+  def apply(cause: Throwable): PaymentErrorException = this(Option(cause).map(_.toString).orNull, cause)
+}
