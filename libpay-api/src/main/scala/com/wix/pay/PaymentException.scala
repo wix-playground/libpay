23899a9f3aaf1201c8712a220e504aa014eb6e6d
Danny Leshem <dleshem@gmail.com>
2017-08-30 11:56:36 +0300

    Added gatewayInternalCode for debugging, logging, and analytics.

--- libpay-api/src/main/scala/com/wix/pay/PaymentException.scala
+++ libpay-api/src/main/scala/com/wix/pay/PaymentException.scala
@@ -7,51 +7,61 @@
 package com.wix.pay
 
 
-/** An exception raised upon failure processing a payment transaction. */
-class PaymentException(message: String, cause: Throwable) extends RuntimeException(message, cause)
+/**
+  * An exception raised upon failure processing a payment transaction.
+  *
+  * @param gatewayInternalCode   an optional, opaque, gateway-specific error code for debugging, logging, and analytics.
+  *                              Do not rely on this for business logic: values and format are treated as implementation
+  *                              detail and may change without notice.
+  */
+class PaymentException(message: String, cause: Throwable, gatewayInternalCode: Option[String] = None) extends RuntimeException(message, cause)
 
 /** The Companion Object of the [[PaymentException]] class, which introduces means for instantiating an exception
   * object.
-  *
-  * @author <a href="mailto:ohadr@wix.com">Raz, Ohad</a>
   */
 object PaymentException {
-  def apply(): PaymentException = new PaymentException(null, null)
-  def apply(message: String): PaymentException = new PaymentException(message, null)
-  def apply(cause: Throwable): PaymentException = new PaymentException(Option(cause).map(_.toString).orNull, cause)
+  def apply(gatewayInternalCode: Option[String] = None): PaymentException = new PaymentException(null, null, gatewayInternalCode)
+  def apply(message: String, gatewayInternalCode: Option[String] = None): PaymentException = new PaymentException(message, null, gatewayInternalCode)
+  def apply(cause: Throwable, gatewayInternalCode: Option[String] = None): PaymentException = new PaymentException(Option(cause).map(_.toString).orNull, cause, gatewayInternalCode)
 }
 
 
 
-/** An exception raised upon rejecting a payment transaction. */
-case class PaymentRejectedException(message: String, cause: Throwable) extends PaymentException(message, cause)
+/**
+  * An exception raised upon rejecting a payment transaction.
+  *
+  * @param gatewayInternalCode   an optional, opaque, gateway-specific error code for debugging, logging, and analytics.
+  *                              Do not rely on this for business logic: values and format are treated as implementation
+  *                              detail and may change without notice.
+  */
+case class PaymentRejectedException(message: String, cause: Throwable, gatewayInternalCode: Option[String] = None) extends PaymentException(message, cause, gatewayInternalCode)
 
 /** The Companion Object of the [[PaymentRejectedException]] class, which introduces means for instantiating an
   * exception object.
-  *
-  * @author <a href="mailto:ohadr@wix.com">Raz, Ohad</a>
   */
 object PaymentRejectedException {
-  def apply(): PaymentRejectedException = this(null, null)
-  def apply(message: String): PaymentRejectedException = this(message, null)
-  def apply(cause: Throwable): PaymentRejectedException = this(Option(cause).map(_.toString).orNull, cause)
+  def apply(gatewayInternalCode: Option[String] = None): PaymentRejectedException = this(null, null, gatewayInternalCode)
+  def apply(message: String, gatewayInternalCode: Option[String] = None): PaymentRejectedException = this(message, null, gatewayInternalCode)
+  def apply(cause: Throwable, gatewayInternalCode: Option[String] = None): PaymentRejectedException = this(Option(cause).map(_.toString).orNull, cause, gatewayInternalCode)
 }
 
 
 
-/** An exception raised upon encountering an error by the payment gateway (e.g., in the case of an invalid
-  * credentials).
+/**
+  * An exception raised upon encountering an error by the payment gateway (e.g., in the case of an invalid credentials).
+  *
+  * @param gatewayInternalCode   an optional, opaque, gateway-specific error code for debugging, logging, and analytics.
+  *                              Do not rely on this for business logic: values and format are treated as implementation
+  *                              detail and may change without notice.
   */
-case class PaymentErrorException(message: String, cause: Throwable) extends PaymentException(message, cause)
+case class PaymentErrorException(message: String, cause: Throwable, gatewayInternalCode: Option[String] = None) extends PaymentException(message, cause, gatewayInternalCode)
 
 
 /** The Companion Object of the [[PaymentErrorException]] class, which introduces means for instantiating an exception
   * object.
-  *
-  * @author <a href="mailto:ohadr@wix.com">Raz, Ohad</a>
   */
 object PaymentErrorException {
-  def apply(): PaymentErrorException = this(null, null)
-  def apply(message: String): PaymentErrorException = this(message, null)
-  def apply(cause: Throwable): PaymentErrorException = this(Option(cause).map(_.toString).orNull, cause)
+  def apply(gatewayInternalCode: Option[String] = None): PaymentErrorException = this(null, null, gatewayInternalCode)
+  def apply(message: String, gatewayInternalCode: Option[String] = None): PaymentErrorException = this(message, null, gatewayInternalCode)
+  def apply(cause: Throwable, gatewayInternalCode: Option[String] = None): PaymentErrorException = this(Option(cause).map(_.toString).orNull, cause, gatewayInternalCode)
 }
